f1d17730028187a4ef8ed0347ed4b16c4fd452a5
Diana Grams <deegrams221@gmail.com>
2020-07-10 11:32:50 -0600

    index.tsx setup

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { AppRegistry } from 'react-native';
+
+// register the app
+AppRegistry.registerComponent('App', () => App);
+
+AppRegistry.runApplication('App', {
+  initialProps: {},
+  rootTag: document.getElementById('root')
+});
+
+
+serviceWorker.unregister();
